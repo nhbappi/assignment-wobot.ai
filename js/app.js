eaa594cf2b50b3aaea3549a5496d115d6873afba
unknown <you@example.com>
2021-12-17 19:01:39 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// const size1 = document.getElementById('size1').element.classList.toggle('size_selection')
+// const size2 = document.getElementById('size2').element.classList.toggle('size_selection');
+// const size3 = document.getElementById('size3').element.classList.toggle('size_selection');
+// const size4 = document.getElementById('size4').element.classList.toggle('size_selection');
+// const size5 = document.getElementById('size5').element.classList.toggle('size_selection');
